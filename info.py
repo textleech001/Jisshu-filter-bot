c57d89ec8657dfd75b492351cf2399b9448ef0a1
textleech001 <textleech@gmail.com>
2025-03-03 13:37:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,10 +18,10 @@ API_HASH = environ.get('API_HASH', '0c58f3e3fecefc4a9d8e5bcf6968a106')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
-USERNAME = environ.get('USERNAME', "https://t.me/") # ADMIN USERNAME
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/JisshuMovieZone')
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', 'Abhi').split()]
+USERNAME = environ.get('Medico_RX', "https://t.me/Medico_RX") # ADMIN USERNAME
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 'https://t.me/c/2334753582/2'))
+MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/BScNursing_EntranceExam')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
